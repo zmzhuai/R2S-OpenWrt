bb5bcad8354a433f4e024ade207f907bf51aec88
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-07-08 22:17:49 +0800

    update luci-app-smartdns

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -138,7 +138,8 @@ git clone -b master --single-branch https://github.com/tty228/luci-app-servercha
 svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/utils/iputils package/network/utils/iputils
 #SmartDNS
 svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/new/smartdns/smartdns
-svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ntlf9t/luci-app-smartdns package/new/smartdns/luci-app-smartdns
+git clone -b master --single-branch https://github.com/pymumu/luci-app-smartdns package/new/smartdns/luci-app-smartdns
+#svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ntlf9t/luci-app-smartdns package/new/smartdns/luci-app-smartdns
 #上网APP过滤
 git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
 #Docker
